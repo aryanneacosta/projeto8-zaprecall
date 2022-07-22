de3dd3d197f5c11f94a03ba22f4da252677e0bfd
Aryanne Acosta <aryanneacosta@gmail.com>
2022-07-22 12:51:59 -0300

    virando a resposta

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,13 +1,48 @@
+import React from "react";
 import "./style.css";
 
-export default function Question({ index, pergunta }) {
+
+export default function Question({ index, pergunta, resposta }) {
+    const [question, setQuestion] = React.useState(false);
+
     return (
-        <li className="aberta"
-            key={index}>
-            <div className="texto">
-                {pergunta}
-            </div>
-            <img src="./././img/setinha.png" alt="setinha" />
-        </li>
+        <>
+            {!question ?
+                <li className="aberta"
+                    key={index}>
+                    <div className="texto">
+                        {pergunta}
+                    </div>
+                    <img
+                        onClick={() => setQuestion(true)}
+                        src="./././img/setinha.png"
+                        alt="setinha"
+                    />
+                </li>
+                :
+                <li className="aberta"
+                    key={index}>
+                    <div className="texto">
+                        {resposta}
+                    </div>
+                    <div className="opções">
+                        <div className="button red">
+                            <div>Não</div>
+                            <div>lembrei!</div>
+                        </div>
+                        <div className="button yellow">
+                            <div>Quase</div>
+                            <div>lembrei!</div>
+                        </div>
+                        <div className="button green">
+                            Zap!
+                        </div>
+
+                    </div>
+                </li>
+            }
+
+        </>
+
     );
 }
